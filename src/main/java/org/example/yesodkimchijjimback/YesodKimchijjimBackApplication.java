e5dfdd33f8c91df8f91ca5567d23e835fb46b3dc
EastRain816 <dder2dder2@gmail.com>
2026-02-03 13:32:58 +0900

    init : 프로젝트 생성

---
+++ src/main/java/org/example/yesodkimchijjimback/YesodKimchijjimBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.yesodkimchijjimback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesodKimchijjimBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YesodKimchijjimBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yesodkimchijjimback/YesodKimchijjimBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yesodkimchijjimback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesodKimchijjimBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
